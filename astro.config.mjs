ef2605b489ee0021f4675af3083d3cedc244aa4a
JessGuo <jesseguoo@gmail.com>
2025-02-07 21:26:52 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
+/*
+ * @Date: 2025-02-07 21:23:45
+ * @LastEditors: JessGuo
+ * @LastEditTime: 2025-02-07 21:26:44
+ * @FilePath: /astro-nano/astro.config.mjs
+ */
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://astro-nano-demo.vercel.app",
+  site: 'https://JesseGuoX.github.io',
+  base: 'blog',
   integrations: [mdx(), sitemap(), tailwind()],
 });
