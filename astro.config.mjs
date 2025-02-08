5f1f0161b441f0be4b7805e0553294d60150af6d
JessGuo <jesseguoo@gmail.com>
2025-02-08 08:33:27 +0800

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 /*
  * @Date: 2025-02-07 21:23:45
  * @LastEditors: JessGuo
- * @LastEditTime: 2025-02-07 21:26:44
+ * @LastEditTime: 2025-02-08 08:33:03
  * @FilePath: /astro-nano/astro.config.mjs
  */
 import { defineConfig } from "astro/config";
@@ -11,6 +11,5 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: 'https://JesseGuoX.github.io',
-  base: 'blog',
   integrations: [mdx(), sitemap(), tailwind()],
 });
